b88a359f012d5ea06c85d06863e46af2ee6fe93e
marcelo-rafael <marcelo.rafael.goncalves@gmail.com>
2021-10-24 14:06:29 -0300

    Componente Layout #01

--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -10,14 +10,19 @@ interface LayoutProps {
 
 export default function Layout(props: LayoutProps) {
   return(
-    <div>
+    <div className={`flex h-screen w-screen`}>
       <MenuLateral />
+      <div className={`
+        flex flex-col w-full p-7
+        bg-gray-300
+      `}>
       <Cabecalho 
         titulo={props.titulo} 
         subtitulo={props.subtitulo} />
       <Conteudo>
         {props.children}  
       </Conteudo>  
+      </div>
     </div>
   )
 }
