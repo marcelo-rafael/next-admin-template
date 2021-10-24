7f854ba47f6d11b909230f131c006da480cd0d22
marcelo-rafael <marcelo.rafael.goncalves@gmail.com>
2021-10-24 13:32:24 -0300

    Estrutura inicial

---
+++ src/components/template/MenuLateral.tsx
@@ -0,0 +1,7 @@
+export default function MenuLateral() {
+  return(
+    <div>
+      Menu
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,9 @@
+import Layout from '../components/template/Layout';
+
 export default function Home() {
   return (
-    <div className={`
-      flex justify-center items-center h-screen
-      bg-gradient-to-r from-green-500 to-blue-500
-    `}>
-      <h1>Admin</h1>
-    </div>
+    <Layout titulo='Página Inicial' subtitulo='Estamos construindo um template Admin'>
+      <h3>Conteúdo</h3>
+    </Layout>
   )
 }
