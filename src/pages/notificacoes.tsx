5848db5012fcbcbcf665e77422bfd1d8b4e3bb79
marcelo-rafael <marcelo.rafael.goncalves@gmail.com>
2021-10-25 02:27:42 -0300

    Context API

--- src/pages/notificacoes.tsx
+++ src/pages/notificacoes.tsx
@@ -1,9 +1,12 @@
 import Layout from '../components/template/Layout';
+import useAppData from '../data/hook/useAppData';
 
 export default function Notificacoes() {
+  const { alternarTema} = useAppData()
+
   return (
     <Layout titulo='Notificações' subtitulo='Aqui você irá gerenciar as suas notificações!'>
-      <h3>Conteúdo</h3>
+      <button onClick={alternarTema}>Click</button>
     </Layout>
   )
 }
